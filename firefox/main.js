30d48945d397d412bcec732e8d438ae08ca4fea0
devBoi76 <mrzyglodjan@protonmail.com>
2023-02-18 16:05:17 +0100

    remove debug info

--- firefox/main.js
+++ firefox/main.js
@@ -1,48 +1,48 @@
 function htmlToElements(html) {
-    var t = document.createElement('template');
-    t.innerHTML = html;
-    return t.content;
+    var t = document.createElement('template')
+    t.innerHTML = html
+    return t.content
 }
 
 function similarity(s1, s2) {
-	var longer = s1;
-	var shorter = s2;
+	var longer = s1
+	var shorter = s2
 	if (s1.length < s2.length) {
-	  longer = s2;
-	  shorter = s1;
+	  longer = s2
+	  shorter = s1
 	}
-	var longerLength = longer.length;
+	var longerLength = longer.length
 	if (longerLength == 0) {
-	  return 1.0;
+	  return 1.0
 	}
-	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
+	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength)
   }
   
 function editDistance(s1, s2) {
-s1 = s1.toLowerCase();
-s2 = s2.toLowerCase();
+s1 = s1.toLowerCase()
+s2 = s2.toLowerCase()
 
 var costs = new Array();
 for (var i = 0; i <= s1.length; i++) {
-	var lastValue = i;
+	var lastValue = i
 	for (var j = 0; j <= s2.length; j++) {
 	if (i == 0)
-		costs[j] = j;
+		costs[j] = j
 	else {
 		if (j > 0) {
 		var newValue = costs[j - 1];
 		if (s1.charAt(i - 1) != s2.charAt(j - 1))
 			newValue = Math.min(Math.min(newValue, lastValue),
 			costs[j]) + 1;
-		costs[j - 1] = lastValue;
-		lastValue = newValue;
+		costs[j - 1] = lastValue
+		lastValue = newValue
 		}
 	}
 	}
 	if (i > 0)
-	costs[s2.length] = lastValue;
+	costs[s2.length] = lastValue
 }
-return costs[s2.length];
+return costs[s2.length]
 }
 
 const new_design_button = '<a id="modrinth-body" href="REDIRECT" target="_blank" style="overflow: hidden; margin-top: -1px; display: flex" > <style>#modrinth-body{height: 36px; text-decoration: none; font-weight: 600; font-family: sans-serif; color: #e5e5e5; --mr-green: #30b27b; transition: none;}#modrinth-body > div{display: flex; align-items: center;}#modrinthify-redirect{display: flex; height: 100%; align-items: center; background-color: var(--mr-green); font-weight: 600; padding-inline: 0.5rem;}#modrinthify-redirect > svg{height: 30px; margin-right: 0.5rem; fill: #e5e5e5;}</style> <div> <img style="display: inline-block; height: 36px; width: 36px" src="ICON_SOURCE"/> <div style="display: inline-block; margin-inline: 1rem; font-weight: 600" > MOD_NAME </div><div id="modrinthify-redirect" data-tooltip="Get on Modrinth" > <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 141.73 141.73" aria-hidden="true" > <g> <path d="M159.07,89.29A70.94,70.94,0,1,0,20,63.52H32A58.78,58.78,0,0,1,145.23,49.93l-11.66,3.12a46.54,46.54,0,0,0-29-26.52l-2.15,12.13a34.31,34.31,0,0,1,2.77,63.26l3.19,11.9a46.52,46.52,0,0,0,28.33-49l11.62-3.1A57.94,57.94,0,0,1,147.27,85Z" transform="translate(-19.79)" fill-rule="evenodd" ></path> <path transform="translate(-19.79)" d="M108.92,139.3A70.93,70.93,0,0,1,19.79,76h12a59.48,59.48,0,0,0,1.78,9.91,58.73,58.73,0,0,0,3.63,9.91l10.68-6.41a46.58,46.58,0,0,1,44.72-65L90.43,36.54A34.38,34.38,0,0,0,57.36,79.75C57.67,80.88,58,82,58.43,83l13.66-8.19L68,63.93l12.9-13.25,16.31-3.51L101.9,53l-7.52,7.61-6.55,2.06-4.69,4.82,2.3,6.38s4.64,4.94,4.65,4.94l6.57-1.74,4.67-5.13,10.2-3.24,3,6.84L104.05,88.43,86.41,94l-7.92-8.81L64.7,93.48a34.44,34.44,0,0,0,28.72,11.59L96.61,117A46.6,46.6,0,0,1,54.13,99.83l-10.64,6.38a58.81,58.81,0,0,0,99.6-9.77l11.8,4.29A70.77,70.77,0,0,1,108.92,139.3Z" ></path> </g> </svg> Get on Modrinth </div></div></a>'
@@ -96,50 +96,36 @@ let mod_name_noloader = undefined
 let page = undefined
 
 function main() {
-	console.log("A")
 	const url = document.URL.split("/")
-	console.log(url)
 	page = url[4]
 	
 	const is_new_design = document.querySelector("#__next") != null
 	
 	const is_search = is_new_design ? (url[4].split("?")[0] == "search") : (url[5].startsWith("search") && url[5].split("?").length >= 2)
 
-	
-
-	console.log(is_new_design)
-
 	if (is_search) {
 		if (is_new_design) {
-			search_query = document.querySelector(".search-input-field").value 
-			console.log("a")
+			search_query = document.querySelector(".search-input-field").value
 		}
 		else {
-			search_query = document.querySelector(".mt-6 > h2:nth-child(1)").textContent.match(/Search results for '(.*)'/)[1];
-			console.log("search")
+			search_query = document.querySelector(".mt-6 > h2:nth-child(1)").textContent.match(/Search results for '(.*)'/)[1]
 		}
 	} else {
 		if (is_new_design) {
 			// search_query = document.querySelector(".project-header > h1:nth-child(2)").innerText
 			search_query = document.querySelector("head > title:nth-child(2)").innerText.split(" - ")[0]
-			console.log(search_query)
 		} else {
 			search_query = document.querySelector("head meta[property='og:title']").getAttribute("content")
 		}
 	}
-
-	console.log("here1", page)
 	
 	mod_name = search_query
 	mod_name_noloader = mod_name.replace(REGEX, "")
-	console.log(mod_name, mod_name_noloader)
 
 	if (is_search && is_new_design) {
 		page_re = /.*&class=(.*?)&.*/
 		page = (page.match(page_re) || ["", "all"])[1]
 	}
-
-	console.log("here1.5", page)
 
 
 	api_facets = ""
@@ -169,11 +155,9 @@ function main() {
 			api_facets = ``
 			break
 	}
-	console.log("fetch")
 	fetch(`https://api.modrinth.com/v2/search?limit=3&query=${mod_name_noloader}&${api_facets}`, {method: "GET", mode: "cors"})
 	.then(response => response.json())
 	.then(resp => {
-		console.log(resp)
 		if (page == undefined) {
 			return
 		}
@@ -198,7 +182,6 @@ function main() {
 		if (max_sim <= 0.7) {
 			return
 		}
-		console.log("here2")
 		// Add the buttons
 		
 		let bd = document.querySelector("#modrinth-body")
@@ -230,7 +213,6 @@ function main() {
 		} else {
 
 			if (is_new_design) {
-				console.log("ADDing new design")
 				query = ".actions"
 				let s = document.querySelector(query)
 				let buttonElement = htmlToElements(new_design_button
@@ -272,7 +254,6 @@ function main() {
 					let dbutton = document.createElement("div")
 					dbutton.innerHTML = DONATE_HTML.replace("REDIRECT", donations[0].url)
 					dbutton.style.display = "inline-block"
-					console.log(dbutton)
 					let redir = document.getElementById("modrinthify-redirect")
 					redir.after(dbutton)
 					if (!is_search) {
