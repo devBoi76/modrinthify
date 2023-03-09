a6b163aec2c6a159da8484f2369170f0f15ad429
devBoi76 <mrzyglodjan@protonmail.com>
2023-03-09 17:52:20 +0100

    Update chrome conversion script to automatically zip extensions

--- convert_chrome.py
+++ convert_chrome.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 
 IN_FOLDER = "firefox"
@@ -21,18 +22,22 @@ for r, d, f in os.walk(ff_path):
         else:
             copy_files.append(os.path.join(r, file))
 
+print("TRANSFORM JS\n")
+
 for f in files:
-    print(f)
+    print("TRANSFORM", f)
 
     with open(f, "r") as ff_file:
         autogen_cr_filename = f.replace("firefox", OUT_FOLDER)
         os.makedirs(os.path.dirname(autogen_cr_filename), exist_ok=True)
-        auto_cr_file = open(autogen_cr_filename, "w+")
         
+        auto_cr_file = open(autogen_cr_filename, "w+")
         auto_cr_file.write(OUT_ADDITIONS)
         ff_text = ff_file.read()
         auto_cr_file.write(ff_text.replace("browser.", "chrome."))
         auto_cr_file.close()
+
+print("\nCOPY SHARED\n")
 
 for f in copy_files:
     print("COPY", f)
@@ -40,3 +45,10 @@ for f in copy_files:
     cr_filename = f.replace("firefox", "chrome")
     os.makedirs(os.path.dirname(autogen_cr_filename), exist_ok=True)
     os.system(f"cp {cr_filename} {autogen_cr_filename}")
+
+print("\nZIP EXTENSIONS\n")
+print(f"{os.getcwd()}/firefox.zip")
+shutil.make_archive(f"{os.getcwd()}/firefox", "zip", root_dir=f"{os.getcwd()}/firefox", base_dir=f"{os.getcwd()}/firefox")
+
+print(f"{os.getcwd()}/chrome.zip")
+shutil.make_archive(f"{os.getcwd()}/chrome", "zip", root_dir=f"{os.getcwd()}/autogen_chrome", base_dir=f"{os.getcwd()}/autogen_chrome")
